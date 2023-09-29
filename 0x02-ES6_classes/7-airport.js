261ac6df5124da9d2a989a820e34e334975cb5cc
Nahom <derebeam2123@email.com>
2023-09-29 08:50:06 -0700

    task all

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,18 +3,23 @@ export default class Airport {
     this.name = name;
     this.code = code;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     this._name = value;
   }
+
   get code() {
     return this._code;
   }
+
   set code(value) {
     this._code = value;
   }
+
   get [Symbol.toStringTag]() {
     return this._code;
   }
